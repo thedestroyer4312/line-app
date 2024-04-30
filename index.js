83fd7fc6b0df5e74a1eb36843e185ac7d33b4874
Trevor Tsai <trevortsai1@gmail.com>
2024-04-30 07:30:36 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const hostname = 'localhost'
+const port = 3000
+
+const server = http.createServer((req, res) => {
+ console.log(req.headers)
+ res.statusCode = 200
+ res.end('<html><body><h1>Hello, there!</h1></body></html>')
+})
+
+server.listen(port, hostname);
